01c4329b4a46a773b3faf9a45c6c79f7d6a47907
Rene Garcia Jr <renegarciajr.@Renes-Air.attlocal.net>
2022-06-17 10:59:34 -0400

    updated registration, post and forgot password

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html","registration.html", "post.html", "forgot_password.html"],
   theme: {
     extend: {},
   },
