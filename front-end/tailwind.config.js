edb3901b93d904a277a8fa4aba560564ca65b6bf
rodolfopoly <rodolfopoly@gmail.com>
2022-06-18 17:30:01 -0400

    front end folder fixed

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","registration.html", "post.html", "forgot_password.html"],
+  content: ["./dist/*.html"],
   theme: {
     extend: {},
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./views/layouts/main.handlebars"],
   theme: {
     extend: {},
   },
