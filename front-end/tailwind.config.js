a9f59da70beb58eae215c77faa2b93b0d0195375
Rene Garcia Jr <renegarciajr.@Renes-Air.attlocal.net>
2022-06-18 09:33:18 -0400

    fixed file structure

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","registration.html", "post.html", "forgot_password.html"],
+  content: ["./dist/index.html","./dist/index.html/registration.html", "./dist/index.html/post.html", "./dist/index.html/forgot_password.html"],
   theme: {
     extend: {},
   },
