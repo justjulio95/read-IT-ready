2a0b14104be5ef715de06ca6e6da6c7baffda822
Rene Garcia Jr <renegarciajr.@Renes-Air.attlocal.net>
2022-06-18 11:49:51 -0400

    finish HTML and Tailwind

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html","./dist/index.html/registration.html", "./dist/index.html/post.html", "./dist/index.html/forgot_password.html"],
+  content: ["./dist/*.html"],
   theme: {
     extend: {},
   },
