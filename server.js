7dd5569b3887401dc14ac630458c4e5554ea9098
rodolfopoly <rodolfopoly@gmail.com>
2022-06-18 20:26:00 -0400

    conected css

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const path = require('path');
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/layouts/main.handlebars"],
+  content: ['./views/layouts/main.handlebars', './views/homepage.handlebars'],
   theme: {
     extend: {},
   },
