3b667e681248b567309bceb272b34e07a2472d3b
Julio.S <just.julio95@gmail.com>
2022-06-18 22:02:03 -0400

    beginnings of handlebars for 'front page' of RIR

--- front-end/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["index.html","registration.html", "post.html", "forgot_password.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
