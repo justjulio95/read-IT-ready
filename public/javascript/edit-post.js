1394464fa47e7a1593794531421c1e14ed502403
Julio.S <just.julio95@gmail.com>
2022-06-21 20:25:16 -0400

    minor fixes to edit, login, and signup functions

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,6 +2,7 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value.trim();
+  const description = document.querySelector('textarea[name="description"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length -1
   ];
@@ -9,7 +10,8 @@ async function editFormHandler(event) {
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title
+      title,
+      description
     }),
     headers: {
       'Content-Type': 'application/json'
